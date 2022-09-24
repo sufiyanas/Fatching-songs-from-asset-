fb34543382aa6cc485d4007936eb9715adc19791
Mohammed sufiyan <mohammedsufiyanas123@gmail.com>
2022-09-24 11:49:13 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/example/fetching_from_file/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fetching_from_file
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:on_audio_query/on_audio_query.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    requestPermission();
+  }
+
+  void requestPermission() {
+    Permission.storage.request();
+  }
+
+  final _audioQuery = OnAudioQuery();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: FutureBuilder<List<SongModel>>(
+        future: _audioQuery.querySongs(
+            sortType: null,
+            orderType: OrderType.ASC_OR_SMALLER,
+            uriType: UriType.EXTERNAL,
+            ignoreCase: true),
+        builder: ((context, item) {
+          if (item.data == null) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (item.data!.isEmpty) {
+            return Text('No Songs FOund');
+          }
+          return ListView.builder(
+            itemBuilder: (context, index) => ListTile(
+              leading: Icon(Icons.abc_outlined),
+              title: Text(item.data![index].displayNameWOExt),
+              subtitle: Text('${item.data![index].artist}'),
+            ),
+            itemCount: item.data!.length,
+          );
+        }),
+      ),
+    );
+  }
+}
